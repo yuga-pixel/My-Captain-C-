0f74e6aee64f31e99e65bb88c1e046c0d6ee5186
yuga-pixel <69682775+yuga-pixel@users.noreply.github.com>
2021-04-28 18:04:03 +0530

    Projects 1 to 5

---
+++ sizeof.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    //PROGRAM 1
+    
+    cout << "Size of char : " << sizeof(char) << " byte" << endl;
+    cout << "Size of int : " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float : " << sizeof(float) <<" byte" << endl;
+    cout << "Size of double : " << sizeof(double) << " byte" << endl;
+    
+    // PROGRAM 2
+    
+    double num1, num2;
+    
+    cout << "Enter two numbers : ";
+    cin >> num1 >> num2;
+    
+    cout << "Product : " << num1*num2 << endl;
+    
+    return 0;
+}
+
+	
+
---
+++ vote.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int age;
+   cout << "How old are you? " << endl;
+   cin >> age;
+   if (age >= 18)
+       cout << "You can vote! " << endl;
+    else
+        cout << "You can't vote! " << endl;
+    return 0;
+}
+
+	
+
